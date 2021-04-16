dbee869d69e0ed9103891d46831b237d39a9e8b1
Frederic Lavigne <frederic.lavigne@ibm.com>
2021-04-16 15:46:15 -0500

    initial checkin

---
+++ 010-prepare-backend/main.tf
@@ -0,0 +1,100 @@
+variable "ibmcloud_api_key" {
+  type = string
+}
+
+variable "ibmcloud_timeout" {
+  type    = number
+  default = 600
+}
+
+variable "region" {
+  type    = string
+  default = "us-south"
+}
+
+variable "basename" {
+  type = string
+  default = "cos-terraform-backend"
+}
+
+variable "resource-group" {
+  type    = string
+  default = ""
+}
+
+variable "tags" {
+  type    = list(string)
+  default = ["terraform", "tutorial"]
+}
+
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
+
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+  ibmcloud_timeout = var.ibmcloud_timeout
+}
+
+# a new or existing resource group to create resources
+resource "ibm_resource_group" "group" {
+  count = var.resource-group != "" ? 0 : 1
+  name  = "${var.basename}-group"
+  tags  = var.tags
+}
+
+data "ibm_resource_group" "group" {
+  count = var.resource-group != "" ? 1 : 0
+  name  = var.resource-group
+}
+
+locals {
+  resource_group_id       = var.resource-group != "" ? data.ibm_resource_group.group.0.id : ibm_resource_group.group.0.id
+}
+
+# a COS instance
+resource "ibm_resource_instance" "cos" {
+  name              = "${var.basename}-cos"
+  resource_group_id = local.resource_group_id
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  location          = "global"
+  tags              = concat(var.tags, ["service"])
+}
+
+resource "ibm_resource_key" "cos_key" {
+  name                 = "${var.basename}-cos-key"
+  resource_instance_id = ibm_resource_instance.cos.id
+  role                 = "Writer"
+
+  parameters = {
+    HMAC = true
+  }
+}
+
+# a bucket
+resource "ibm_cos_bucket" "bucket" {
+  bucket_name          = "${var.basename}-bucket"
+  resource_instance_id = ibm_resource_instance.cos.id
+  region_location      = var.region
+  storage_class        = "smart"
+}
+
+resource "local_file" "ansible-config" {
+  content = templatefile("../020-use-backend/backend.tf.tmpl",
+    {
+      bucket = ibm_cos_bucket.bucket.bucket_name
+      endpoint = ibm_cos_bucket.bucket.s3_endpoint_public
+      access_key = ibm_resource_key.cos_key.credentials["cos_hmac_keys.access_key_id"]
+      secret_key = ibm_resource_key.cos_key.credentials["cos_hmac_keys.secret_access_key"]
+      region = var.region
+    }
+  )
+  filename = "../020-use-backend/backend.tf"
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Using Cloud Object Storage as a backend
+
+This repository shows how to use IBM Cloud Object Storage as a backend to store terraform state through [Terraform S3 compatibility](https://www.terraform.io/docs/backends/types/s3.html).
+
+1. Create a COS instance and bucket in IBM Cloud
+   ```sh
+   cd 010-prepare-backend
+   terraform init
+   terraform apply
+   ```
+
+   > It generates a `backend.tf` file in `020-use-backend` with the COS service instance and bucket information. Later instead of hardcoding the values in `backend.tf`, you can use environment variables to initialize the backend as described in https://www.terraform.io/docs/language/settings/backends/s3.html.
+
+2. Test the backend
+   ```sh
+   cd 020-use-backend
+   terraform init
+   terraform apply
+   ```
+
+3. Look into your COS bucket for a file named `global.state`.
+
+## License
+
+This project is licensed under the Apache License Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
